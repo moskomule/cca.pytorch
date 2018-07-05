1106dccf6a15e1fe6464ea9fa2751ebab6adf1f7
moskomule <hataya@keio.jp>
2018-07-05 13:04:19 +0900

    fix

--- src/example.py
+++ src/example.py
@@ -35,8 +35,8 @@ def main(batch_size):
     train_loader, test_loader = get_loader(128)
     model1 = resnet20(num_classes=10)
     model2 = preact_resnet20(num_classes=10)
-    hook = CCAHook([model1, model2], [["layer1.0.conv1", "conv2.0.conv1", "layer3.0.conv1"],
-                                      ["layer1.0.conv1", "conv2.0.conv1", "layer3.0.conv1"]],
+    hook = CCAHook([model1, model2], [["layer1.0.conv1", "layer2.0.conv1", "layer3.0.conv1"],
+                                      ["layer1.0.conv1", "layer2.0.conv1", "layer3.0.conv1"]],
                    train_loader.dataset, batch_size=batch_size)
     optimizer1 = torch.optim.SGD(params=model1.parameters(), lr=0.1, momentum=0.9, weight_decay=1e-4)
     scheduler1 = torch.optim.lr_scheduler.StepLR(optimizer1, 50)
